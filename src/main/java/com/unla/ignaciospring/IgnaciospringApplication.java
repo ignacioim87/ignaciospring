cd572fd2a436238297673c866dfc18ac61e43e62
ignacioim87 <ignaciom8719@gmail.com>
2020-10-27 15:45:27 -0300

    adding spring files

---
+++ src/main/java/com/unla/ignaciospring/IgnaciospringApplication.java
@@ -0,0 +1,13 @@
+package com.unla.ignaciospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgnaciospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgnaciospringApplication.class, args);
+	}
+
+}
