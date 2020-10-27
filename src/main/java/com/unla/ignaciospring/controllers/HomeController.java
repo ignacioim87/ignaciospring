34fec0296834ed09e5c7e96866848aa8bcd04f8f
ignacioim87 <ignaciom8719@gmail.com>
2020-10-27 17:40:22 -0300

    added degree

--- src/main/java/com/unla/ignaciospring/controllers/HomeController.java
+++ src/main/java/com/unla/ignaciospring/controllers/HomeController.java
@@ -1,12 +1,18 @@
 package com.unla.ignaciospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+
+import com.unla.ignaciospring.helpers.ViewRouteHelper;
+import com.unla.ignaciospring.models.Degree;
 
 @Controller
 @RequestMapping("/")
@@ -14,28 +20,42 @@ public class HomeController {
 	// GET example: SERVER/index
 	@GetMapping("/index")
 	public String index() {
-		return "home/index";
+		return ViewRouteHelper.INDEX;
 	}
 
 	// GET example: SERVER/hello?name=someName
 	@GetMapping("/hello")
-	public ModelAndView helloparams1(@RequestParam(name="name", required=false, defaultValue="null") String name) {
-		ModelAndView mV = new ModelAndView("home/hello");
+	public ModelAndView helloParams1(@RequestParam(name="name", required=false, defaultValue="null") String name) {
+		ModelAndView mV = new ModelAndView(ViewRouteHelper.HELLO);
 		mV.addObject("name", name);
 		return mV;
 	}
-
+	
 	// GET example: SERVER/hello/somename
 	@GetMapping("/hello/{name}")
-	public ModelAndView helloparams2(@PathVariable("name") String name) {
-		ModelAndView mV = new ModelAndView("home/hello");
+	public ModelAndView helloParams2(@PathVariable("name") String name) {
+		ModelAndView mV = new ModelAndView(ViewRouteHelper.HELLO);
 		mV.addObject("name", name);
 		return mV;
 	}
 
 	@GetMapping("/")
 	public RedirectView redirectToHomeIndex() {
-		return new RedirectView("home/index");
+		return new RedirectView(ViewRouteHelper.ROUTE_INDEX);
+	}
+	
+	@GetMapping("/degree")
+	public String degree(Model model) {
+		model.addAttribute("degree", new Degree());
+		return ViewRouteHelper.DEGREE;
+	}
+	
+	@PostMapping("/newdegree")
+	public ModelAndView newdegree(@ModelAttribute("degree") Degree degree) {
+		ModelAndView mV= new ModelAndView();
+		mV.setViewName(ViewRouteHelper.NEW_DEGREE);
+		mV.addObject("degree",degree);
+		return mV;
 	}
 
 }
---
+++ src/main/java/com/unla/ignaciospring/helpers/ViewRouteHelper.java
@@ -0,0 +1,13 @@
+package com.unla.ignaciospring.helpers;
+
+public class ViewRouteHelper {
+	//Views
+	public final static String INDEX="home/index";
+	public final static String HELLO="home/hello";
+	public final static String DEGREE="home/degree";
+	public final static String NEW_DEGREE="home/newdegree";
+	
+	//Redirects
+	public final static String ROUTE_INDEX="/index";
+	
+}	
