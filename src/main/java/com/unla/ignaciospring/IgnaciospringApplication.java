e4ffe138c9c921cf25074c07755b63ff8ab56e84
ignacioim87 <ignaciom8719@gmail.com>
2020-10-27 16:34:42 -0300

    added controller and views

--- src/main/java/com/unla/ignaciospring/IgnaciospringApplication.java
+++ src/main/java/com/unla/ignaciospring/IgnaciospringApplication.java
@@ -2,8 +2,10 @@ package com.unla.ignaciospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.unla.ignaciospring.controllers"})
 public class IgnaciospringApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/unla/ignaciospring/controllers/HomeController.java
@@ -0,0 +1,42 @@
+package com.unla.ignaciospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	// GET example: SERVER/index
+	@GetMapping("/index")
+	public String index() {
+		return "home/index";
+	}
+
+	// GET example: SERVER/hello?name=someName
+	@GetMapping("/hello")
+	public ModelAndView helloparams1(
+			@RequestParam(name = "name", required = false, defaultValue = "null") String name) {
+		ModelAndView mV = new ModelAndView("/home/hello");
+		mV.addObject("name", name);
+		return mV;
+	}
+
+	// GET example: SERVER/hello/somename
+	@GetMapping("/hello/{name}")
+	public ModelAndView helloparams2(@PathVariable("name") String name) {
+		ModelAndView mV = new ModelAndView("/home/hello");
+		mV.addObject("name", name);
+		return mV;
+	}
+
+	@GetMapping("/")
+	public RedirectView redirectToHomeIndex() {
+		return new RedirectView("home/index");
+	}
+
+}
